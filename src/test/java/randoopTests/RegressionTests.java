d1d593bc137ae46898405b3aad507624ec4e7bf3
Valentino Natali <valenatali0347@gmail.com>
2026-09-17 13:42:14 -0300

    test: Restore randoop regression test suite

---
+++ src/test/java/randoopTests/RegressionTests.java
@@ -0,0 +1,14 @@
+package randoopTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+    BoardRegressionTest.class,
+    CellRegressionTest.class 
+})
+public class RegressionTests{
+
+}
+
