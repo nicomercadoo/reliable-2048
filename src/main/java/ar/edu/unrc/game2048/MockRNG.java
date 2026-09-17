f1ff99c0ed5aee1fad57e19be0c0d1dffb9cf859
Nicolás Mercado <nicolasmercado452@gmail.com>
2026-09-17 14:19:44 -0300

    feat(MockRNG): add check for constructor parameter

--- src/main/java/ar/edu/unrc/game2048/MockRNG.java
+++ src/main/java/ar/edu/unrc/game2048/MockRNG.java
@@ -8,8 +8,11 @@ public class MockRNG implements RNGStrategy {
         this.values = new double[]{0.5};
         this.index = 0;
     }
-    
+
     public MockRNG(double val) {
+        if (val < 0 || val > 1) {
+            throw new IllegalArgumentException("Value must be between 0 and 1");
+        }
         this.values = new double[]{val};
         this.index = 0;
     }
